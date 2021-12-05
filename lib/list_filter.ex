acd6019514a55c80698398535eac683f067ec521
azevedorafael <rafaelsilva.fatec@gmail.com>
2021-12-05 18:45:23 -0300

    feat(list-filter): filter a given list and return the total amount of odd elements

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,17 @@
+require Integer
+
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  def call(list), do: filter(list)
 
-  @doc """
-  Hello world.
+  defp filter([]), do: []
 
-  ## Examples
+  defp filter(list) do
+    Enum.filter(list, fn element -> is_number(element) end)
+    |> count_odd
+  end
 
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp count_odd(list) do
+    Enum.filter(list, fn x -> rem(x, 2) == 1 end)
+    |> Enum.count()
   end
 end
