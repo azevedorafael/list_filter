78f4167149e2f5f4e11c7844f043c1d93824cd22
azevedorafael <rafaelsilva.fatec@gmail.com>
2021-12-05 18:48:42 -0300

    test(list-filter): filter a given list and return the total amount of odd elements

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,15 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "returns the total of odd elements from a given list" do
+      list = [1, 2, 3, "test", 4, 5, "0", 9]
+
+      response = ListFilter.call(list)
+
+      expected_response = 4
+
+      assert response == expected_response
+    end
   end
 end
